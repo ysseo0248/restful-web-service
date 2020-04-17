28fad21c65147c7820f33c28de32aed8f2248036
ysseo <ysseo0248@gmail.com>
2020-04-17 16:50:45 +0900

    Initial commit

---
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestfulWebServiceApplication.class, args);
+		System.out.println("ㄴㄷㄹㄴㄷㄹㄴㄷㄹ");
+	}
+
+}
