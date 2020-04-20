993f61bd9da787d2da818f7b941d3dd8dbbd3956
ysseo <ysseo0248@gmail.com>
2020-04-20 15:24:29 +0900

    feat : http status code 제어 추가

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,8 +1,11 @@
 package com.example.restfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,7 +27,19 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user){
         User savedUser = service.save(user);
+
+        //http status code 제어
+        URI location = ServletUriComponentsBuilder
+                            .fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(savedUser.getId())
+                            .toUri();
+
+        return ResponseEntity.created(location).build();
     }
+
+
+
 }
